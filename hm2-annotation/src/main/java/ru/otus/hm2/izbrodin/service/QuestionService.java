a7376be6183ffbeec8e48e929a78f1bbb94ea65f
IzbrodinA <izbrodin90@gmial.com>
2020-04-02 00:42:43 +0300

    Add hm-2

---
+++ hm2-annotation/src/main/java/ru/otus/hm2/izbrodin/service/QuestionService.java
@@ -0,0 +1,10 @@
+package ru.otus.hm2.izbrodin.service;
+
+import ru.otus.hm2.izbrodin.domain.Question;
+
+import java.util.List;
+
+public interface QuestionService {
+
+    List<Question> getAllQuestion();
+}
---
+++ hm2-annotation/src/main/java/ru/otus/hm2/izbrodin/service/QuizService.java
@@ -0,0 +1,6 @@
+package ru.otus.hm2.izbrodin.service;
+
+public interface QuizService {
+
+    void startQuiz();
+}
